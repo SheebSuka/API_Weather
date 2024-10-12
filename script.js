478f130259efe704224f5c4b29d2e9fbda9d8f5a
Sheeb <thingisss89@gmail.com>
2024-10-12 00:44:36 -0700

    Test Commit

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
 const apiKey = '9b9a43120a8fe9f9a9edde94b15d8513';
-const city = 'Texas';
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+const ciudadInput = document.getElementById('ciudad');
+const botonBuscar = document.getElementById('buscar');
+const resultado = document.getElementById('resultado');
 
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        const temperature = data.main.temp;
-        const humidity = data.main.humidity;
-        const weatherDescription = data.weather[0].description;
-        const city_name = data.name;
-        const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+botonBuscar.addEventListener('click', buscarCiudad);
 
-        const cityNameElement = document.getElementById('city-name');
-        const temperatureElement = document.getElementById('temperature');
-        const humidityElement = document.getElementById('humidity');
-        const weatherDescriptionElement = document.getElementById('weather-description');
-        const weatherIconElement = document.getElementById('weather-icon');
+function buscarCiudad() {
+    const ciudad = ciudadInput.value.trim();
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}`;
 
-        cityNameElement.textContent = city_name;
-        temperatureElement.textContent = `Temperature: ${temperature}°C`;
-        humidityElement.textContent = `Humidity: ${humidity}%`;
-        weatherDescriptionElement.textContent = `Weather: ${weatherDescription}`;
-        weatherIconElement.src = iconUrl;
-    })
-    .catch(error => console.error(error));
+    fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Ciudad no existente');
+            }
+        })
+        .then(data => {
+            const temperatura = data.main.temp;
+            const humedad = data.main.humidity;
+            const descripcionClima = data.weather[0].description;
+            const nombreCiudad = data.name;
+            const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+
+            resultado.innerHTML = `
+        <h1>${nombreCiudad}</h1>
+        <h2>Temperatura: ${temperatura}°C</h2>
+        <p>Humedad: ${humedad}%</p>
+        <p>Clima: ${descripcionClima}</p>
+        <img src="${iconUrl}" alt="Icono del clima">
+      `;
+        })
+        .catch(error => {
+            resultado.innerHTML = `
+        <h1>Ciudad no existente</h1>
+        <p>Intenta nuevamente</p>
+      `;
+        });
+}
